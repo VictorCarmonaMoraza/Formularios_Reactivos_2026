6b17056b14b294de8dcad68a646a168e58b4797c
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-13 13:22:32 +0100

    Fomr Builder

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -10,8 +10,19 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class BasicPage {
 
-  basicForm = new FormGroup({
-    name: new FormControl(''),
+  //private  para que no se pueda modificar desde fuera de la clase
+  private fb = inject(FormBuilder);
+
+  //Forma 1 de formularios (RECOMENDADA)
+  basicForm = this.fb.group({
+    name: ['',/**Validadores sincronos */[],/**Validadores asincronos */[]],
+    price: [0],
+    inStorage: [0],
+  })
+
+  //Fomra 2 de formularios
+  basicForm2 = new FormGroup({
+    name: new FormControl('',/**Validadores sincronos */[],/**Validadores asincronos */[]),
     price: new FormControl(0),
     inStorage: new FormControl(0),
   })
