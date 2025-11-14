68b11d57dcc425288f00d542914192d0b62b9f55
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-14 12:23:57 +0100

    Switches,Checks y Radio Buttons

--- src/app/reactive/pages/switches-page/switches-page.ts
+++ src/app/reactive/pages/switches-page/switches-page.ts
@@ -1,12 +1,28 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.html',
   styleUrl: './switches-page.css',
 })
 export class SwitchesPage {
 
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  public switchesForm = this.fb.group({
+    gender: [, Validators.required],
+    wantNotidications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  })
+
+
+  onSubmit() {
+    this.switchesForm.markAllAsTouched();
+    console.log(this.switchesForm.value);
+  }
 }
