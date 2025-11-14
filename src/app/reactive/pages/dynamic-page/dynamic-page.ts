5c5312f77356dc17c4e888979dfa97c612a5ea74
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-14 08:26:37 +0100

    Formularios dinamicos con arreglos

--- src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,12 +1,29 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.html',
   styleUrl: './dynamic-page.css',
 })
 export class DynamicPage {
+  private fb = inject(FormBuilder);
+
+  dynamicForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoritesGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ], Validators.minLength(3))
+
+
+  });
+
+  get favoritesGames() {
+    return this.dynamicForm.get('favoritesGames') as FormArray;
+  }
+
 
 }
