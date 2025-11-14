5f7eba2e17172323f0816eea5992c5c86ff9bb28
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-14 20:20:35 +0100

    Suscribirse a los cambios del formulario

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 import { CountryService } from '../../services/country-service';
 import { Country } from '../../interfaces/country-interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -23,4 +24,32 @@ export class CountryPage {
     country: ['', Validators.required],
     border: ['', Validators.required],
   });
+
+  onFormChanged = effect((onCleanup) => {
+    const regionSubscription = this.onRegionChanged();
+
+    onCleanup(() => {
+      regionSubscription.unsubscribe();
+      console.log('Limpiado')
+    })
+  });
+
+  onRegionChanged() {
+    return this.countryForm.get('region')!.valueChanges
+      .pipe(
+        tap((region) => this.countryForm.get('country')!.setValue('')),
+        tap((region) => this.countryForm.get('border')!.setValue('')),
+        tap((region) => {
+          this.borders.set([]);
+          this.countryByRegion.set([]);
+        }),
+        switchMap(region => this.countryService.getCountriesByRegion(region!))
+      )
+      .subscribe(countries => {
+        console.log(countries);
+        this.countryByRegion.set(countries);
+      });
+  }
+
+
 }
