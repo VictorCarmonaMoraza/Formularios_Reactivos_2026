edbf2d5115c842fad24f1474789d40794746a21f
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-14 19:58:19 +0100

    Primer selector controlado

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { CountryService } from '../../services/country-service';
+import { Country } from '../../interfaces/country-interface';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './country-page.html',
   styleUrl: './country-page.css',
 })
 export class CountryPage {
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
 
+  regions = signal(this.countryService.region);
+  countryByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  countryForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
 }
